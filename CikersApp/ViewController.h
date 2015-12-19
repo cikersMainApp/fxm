57ebe92fdc57245da7cc54c54ede0f983d42a027
Ximin Fu <xinmin.fu@cikers.com>
2015-12-19 20:28:30 +0800

    Initial Commit

---
+++ CikersApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CikersApp
+//
+//  Created by fxm on 15/12/19.
+//  Copyright © 2015年 cikers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CikersAppUITests/CikersAppUITests.m
@@ -0,0 +1,40 @@
+//
+//  CikersAppUITests.m
+//  CikersAppUITests
+//
+//  Created by fxm on 15/12/19.
+//  Copyright © 2015年 cikers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CikersAppUITests : XCTestCase
+
+@end
+
+@implementation CikersAppUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
